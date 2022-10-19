0f9c06bb7799338a503469a4278fca40aed4f10f
Sam Shen <slshen@users.noreply.github.com>
2022-10-19 12:27:51 -0700

    Add tournament data

--- pkg/dataexport/game.go
+++ pkg/dataexport/game.go
@@ -1,36 +1,49 @@
 package dataexport
 
 import (
+	"github.com/slshen/sb/pkg/boxscore"
 	"github.com/slshen/sb/pkg/dataframe"
 	"github.com/slshen/sb/pkg/game"
 )
 
 type Game struct {
-	GameID       string
-	GameNumber   string
-	GameDate     string
-	Tournament   string
-	TournamentID string
-	Home         string
-	Visitor      string
-	HomeScore    int
-	VisitorScore int
+	GameID        string
+	GameNumber    string
+	GameDate      string
+	Tournament    string
+	TournamentID  string
+	Home          string
+	Visitor       string
+	HomeScore     int
+	HomeHits      int
+	HomeErrors    int
+	VisitorScore  int
+	VisitorHits   int
+	VisitorErrors int
 }
 
 type Games []*Game
 
-func newGame(g *game.Game, tournamentID string) *Game {
-	return &Game{
-		GameID:       getGameID(g),
-		GameNumber:   g.Number,
-		GameDate:     g.GetDate().Format("2006-01-02"),
-		Tournament:   g.Tournament,
-		TournamentID: tournamentID,
-		Home:         g.Home.Name,
-		Visitor:      g.Visitor.Name,
-		HomeScore:    g.Final.Home,
-		VisitorScore: g.Final.Visitor,
+func newGame(g *game.Game, tournamentID string) (*Game, error) {
+	box, err := boxscore.NewBoxScore(g, nil)
+	if err != nil {
+		return nil, err
 	}
+	return &Game{
+		GameID:        getGameID(g),
+		GameNumber:    g.Number,
+		GameDate:      g.GetDate().Format("2006-01-02"),
+		Tournament:    g.Tournament,
+		TournamentID:  tournamentID,
+		Home:          g.Home.Name,
+		HomeScore:     g.Final.Home,
+		HomeHits:      box.HomeLineup.TotalHits(),
+		HomeErrors:    box.HomeLineup.Errors,
+		Visitor:       g.Visitor.Name,
+		VisitorScore:  g.Final.Visitor,
+		VisitorHits:   box.VisitorLineup.TotalHits(),
+		VisitorErrors: box.VisitorLineup.Errors,
+	}, nil
 }
 
 func (games Games) GetData() *dataframe.Data {
